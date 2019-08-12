49a32ddaaa44b05c1fe44fa0b6623a255803f8e9
keyus <m13820915363@163.com>
2019-08-12 23:00:21 +0800

    重新编写了gateway模块的路由配置

--- keyuspan-gateway/src/main/java/org/keyus/project/keyuspan/KeyuspanGatewayServer.java
+++ keyuspan-gateway/src/main/java/org/keyus/project/keyuspan/KeyuspanGatewayServer.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author keyus
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableFeignClients
 public class KeyuspanGatewayServer {
 
     public static void main(String[] args) {
